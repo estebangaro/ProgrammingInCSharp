14c48c8556553d461a43c18df65b7418dfa08313
esteban_net_vm <38247804+estebanet@users.noreply.github.com>
2019-01-05 00:47:40 -0600

    Se incluyen ejercicios de parallelloopstate y parallelloopresult

--- Exam Ref 70-483/Task Parallel library/net/Parallel.Invoke/ParallelLoopState-Result/Program.cs
+++ Exam Ref 70-483/Task Parallel library/net/Parallel.Invoke/ParallelLoopState-Result/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,41 @@ namespace ParallelLoopState_Result
     {
         static void Main(string[] args)
         {
+            var items = Enumerable.Range(0, 500);
+            ParallelLoopResult result;
+
+            using (var FileStream = File.OpenWrite("break_loop.txt"))
+            {
+                using (StreamWriter Sw = new StreamWriter(FileStream))
+                {
+                    result = Parallel.For(0, items.Count(),
+                        (int index, ParallelLoopState ParallelLoopState) =>
+                    {
+                        if (index == 200)
+                        {
+                            ParallelLoopState.Break();
+                        }
+
+                        WorkOnItem(index, Sw);
+                    });
+                }
+            }
+
+            Console.WriteLine("Completed: " + result.IsCompleted);
+            Console.WriteLine("Items: " + result.LowestBreakIteration);
+            Console.WriteLine("Finish procesing. Press any key to end.");
+            Console.ReadKey();
+        }
+
+        static void WorkOnItem(object item, StreamWriter Console)
+        {
+            Console.WriteLine("Starting working on: " + item);
+            System.Console.WriteLine("Starting working on: " + item);
+
+            System.Threading.Thread.Sleep(100);
+
+            Console.WriteLine("Finished working on: " + item);
+            System.Console.WriteLine("Finished working on: " + item);
         }
     }
 }
