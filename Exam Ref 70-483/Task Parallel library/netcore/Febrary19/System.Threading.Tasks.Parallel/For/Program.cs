9b30a9eb2e60378dbecec99dc4aac064a4f62501
Ubuntu <estebangaro@outlook.com>
2019-02-21 04:39:28 +0000

    Se incluyen ejemplos de ForEach y For

---
+++ Exam Ref 70-483/Task Parallel library/netcore/Febrary19/System.Threading.Tasks.Parallel/For/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static bool Es499;
+        static void Main(string[] args)
+        {
+            /*Observaciones:
+            1) El método For de Parallel, carece de las secciones de evaluación de condición de continuación
+                así como de la sección incrementos sobre la variable de control a diferencia de la estructura
+                tradicional.
+            2) Se compone de 12 sobrecargas, entre las cuales se puede hacer uso de objetos como ParallelLoopState,
+                ParallelLoopResult y delegados para generación de valores ThreadLocal.
+            3) Al igual que sus hermana ForEach, no concluye su ejecución, hasta que todos los elementos
+                hayan sido procesados ó 1 o más procesos generen excepciones no controladas.
+             */
+             Es499 = false;
+            Console.WriteLine("Inciando demostración de método For");
+            try{
+            System.Threading.Tasks.ParallelLoopResult ResultState 
+                = System.Threading.Tasks.Parallel.For(0, 500, WorkOnItem);
+                }catch(AggregateException ae){
+
+                }
+            Console.WriteLine("Finalizando la ejecución del método For, presione una tecla para continuar");
+            Console.WriteLine("Se proceso elemento 499? " + Es499);
+            Console.ReadKey();
+        }
+
+        static void WorkOnItem(int item){
+            if(item == 499){
+                Es499 = true;
+            }
+            Console.WriteLine("Iniciando trabajo en " + item);
+            System.Threading.Thread.Sleep(100);
+            if(item == 325){
+                throw new Exception("Excepción en item " + item);
+            }
+            Console.WriteLine("Finalizando trabajo en " + item);
+        }
+    }
+}
---
+++ Exam Ref 70-483/Task Parallel library/netcore/Febrary19/System.Threading.Tasks.Parallel/ForEach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Observaciones:
+            1) El delegado Action especificado, como argumento para la invocación de ForEach,
+                no puede especificar un tipo Base (ej.Object VS int) como tipo cerrado del delegado.
+            2) El método ForEach cuenta con 20 sobrecargas.
+            3) *Queda pendiente el uso de las 19 sobrecargas restantes, hasta la revisión, de
+                los objetos ParallelLoopState, ParallelLoopResult y ThreadLocal.
+            4) A diferencia del método Invoke, el método ForEach detiene prematuramente la ejecución
+                del procesamiento pendiente de elementos de la colección si se desencadena
+                1 o más excepciones no controladas.
+             */
+            Console.WriteLine("Iniciando demostración de método ForEach");
+            System.Threading.Tasks.ParallelLoopResult ResultState 
+                = System.Threading.Tasks.Parallel.ForEach(System.Linq.Enumerable.Range(0, 500), 
+                WorkOnItem);
+            Console.WriteLine("Finalizando proceso, presione una tecla para continuar");
+            Console.ReadKey();
+        }
+
+        static void WorkOnItem(int item){
+            try{
+            Console.WriteLine("Iniciando trabajo en " + item);
+            System.Threading.Thread.Sleep(100);
+            if(item == 110){
+                throw new Exception("Excepción en item " + 100);
+            }
+            Console.WriteLine("Finalizando trabajo en " + item);}
+            catch{
+                Console.WriteLine("Manejando excepción en item: " + 100);
+            }
+        }
+    }
+}
