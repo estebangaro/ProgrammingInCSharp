4d41d1a88399c4df3e456da886f17f6f131ea039
Ubuntu <estebangaro@outlook.com>
2019-02-22 04:51:32 +0000

    Se incluyen ejemplos de Managing de For y ForEach de Tasks.Parallel.

---
+++ Exam Ref 70-483/Task Parallel library/netcore/Febrary19/System.Threading.Tasks.Parallel/Managing For & ForEach/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Managing_For___ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Observaciones:
+            1)*Concluir verificación de funcionamiento de métodos Stop / Break.
+            2)*Concluir la verificación de las 2 propiedades de ParallelLoopResult 
+                que coincidan con la invocación de los métodos Stop / Break.
+            3)*Concluir verificación de uso de valor de propiedad IsExceptional, pues el comportamiento
+                obtenido durante los ejercicios previos, es que las iteraciones cesan una vez
+                que se genera 1 o más excepciones.
+            4)*Considerar la revisión de las sobrecargas de For / ForEach (debido a la documentación,
+                en ParallelLoopOptions, ParallelLoopResult y ParallelLoopState).
+             */
+            System.Threading.Tasks.ParallelOptions options = new System.Threading.Tasks.ParallelOptions();
+            options.MaxDegreeOfParallelism = 3;
+            Console.WriteLine("Iniciando demo de administración de ciclos paralelos.");
+            System.Threading.Tasks.ParallelLoopResult Result = System.Threading.Tasks.Parallel.For(0, 500, 
+             options,
+             delegate(int index, 
+                System.Threading.Tasks.ParallelLoopState parallelLoopState){
+                    if(index == 199){
+                        parallelLoopState.Stop();
+                    }
+                    WorkOnItem(index, parallelLoopState);
+                });
+            Console.WriteLine("Finalizando ejecución de \"demo de administración de ciclos paralelos\".");
+            Console.WriteLine($"Estado de ParallelLoopResult: IsCompleted = {Result.IsCompleted}, "
+                + $"LowestBreakIteration = {Result.LowestBreakIteration}");
+        }
+
+        static void WorkOnItem(int item, 
+            System.Threading.Tasks.ParallelLoopState parallelLoopState){
+            
+            Console.WriteLine("Iniciando trabajo en " + item);
+            System.Threading.Thread.Sleep(300);
+            if(!parallelLoopState.IsStopped){
+                Console.WriteLine("Finalizando trabajo en " + item);
+            }else{
+                Console.WriteLine($"Finalizando por solicitud de cancelación, en item#{item}");
+                Console.WriteLine($"Valor de ShouldExitCurrentIteration:{parallelLoopState.ShouldExitCurrentIteration}; item#{item}");
+            }
+        }
+    }
+}
