2f716a76544424eaa43e8498e2adbe4bb6680fa4
Ubuntu <estebangaro@outlook.com>
2019-02-27 22:50:21 +0000

    Se incluyen ejemplos de Informing Parallelization

--- Exam Ref 70-483/Task Parallel library/netcore/Febrary19/PLINQ/AsParallel/Program.cs
+++ Exam Ref 70-483/Task Parallel library/netcore/Febrary19/PLINQ/AsParallel/Program.cs
@@ -6,6 +6,11 @@ namespace AsParallel
     class Program
     {
         class Person{
+
+            public Person(){
+                System.Threading.Thread.Sleep(100);
+            }
+
             public string Name { get; set; }
             public int Age { get; set; }
             public string City { get; set; }
@@ -33,6 +38,7 @@ namespace AsParallel
                 5.4) WithMergeOptions, permite especificar el modo en el que serán amortiguados los resultados
                     de salida (AutoBuffered, Default, FullyBuffered, NotBuffered), aunque estos valores son
                     considerados meramente como sugerencias de fusión.
+            6) *Pendiente ejecutar ejemplo con AsSequential.
              */
             Console.WriteLine("Iniciando demo de PLINQ!");
             Person[] Persons = new Person[]{
@@ -40,9 +46,32 @@ namespace AsParallel
                 new Person { Name = "Esteban", Age = 27, City = "México", DateOfBirth = new DateTime(1991, 9, 25)},
                 new Person { Name = "Bárbara", Age = 0, City = "México", DateOfBirth = new DateTime(2019, 9, 11)},
                 new Person { Name = "Arturo", Age = 0, City = "México", DateOfBirth = new DateTime(2019, 9, 12)},
-                new Person { Name = "Rocio", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)}
+                new Person { Name = "Rocio", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Jacquita", Age = 13, City = "Panama", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Luis", Age = 23, City = "Nicaragua", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Samantha", Age = 23, City = "E.U.A", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Elias", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Pepe", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Ámbar2", Age = 24, City = "México", DateOfBirth = new DateTime(1994, 12, 24)},
+                new Person { Name = "Esteban2", Age = 27, City = "México", DateOfBirth = new DateTime(1991, 9, 25)},
+                new Person { Name = "Bárbara2", Age = 0, City = "México", DateOfBirth = new DateTime(2019, 9, 11)},
+                new Person { Name = "Arturo2", Age = 0, City = "México", DateOfBirth = new DateTime(2019, 9, 12)},
+                new Person { Name = "Rocio2", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Jacquita2", Age = 13, City = "Panama", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Luis2", Age = 23, City = "Nicaragua", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Samantha2", Age = 23, City = "E.U.A", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Elias2", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)},
+                new Person { Name = "Pepe2", Age = 47, City = "México", DateOfBirth = new DateTime(1981, 8, 11)}
             };
 
+            //UseAsParallel(Persons);
+            UseInformingParallelization(Persons);
+
+            Console.WriteLine("Finalizando demo, de método AsParallel...");
+            Console.ReadKey();
+        }
+
+        static void UseAsParallel(System.Collections.Generic.IEnumerable<Person> Persons){
             var PersonsOfMexico = from person in Persons.AsParallel()
                                     where person.City == "México"
                                     select person.Name;
@@ -52,9 +81,131 @@ namespace AsParallel
             {
                 Console.WriteLine(PersonName);
             }
+        }
 
-            Console.WriteLine("Finalizando demo, de método AsParallel...");
-            Console.ReadKey();
+        static void UseInformingParallelization(System.Collections.Generic.IEnumerable<Person> Persons){
+            //UseWithDegreeOfParallelism(Persons);
+            //UseWithCancellation(Persons);
+            //UseWithExecutionMode(Persons);
+            //UseAsOrdered(Persons);
+            UseLinq(Persons);
+        }
+
+        static void UseAsOrdered(System.Collections.Generic.IEnumerable<Person> Persons){
+            Console.WriteLine("Iniciando demo de AsOrdered...");
+
+            var PersonsOfMexico = Persons.AsParallel()
+                                    .AsOrdered()
+                                    .WithDegreeOfParallelism(3)
+                                    .Where(person => { ShowThreadCurrentInfo("\"Where city (plinq)\"");
+                                        return person.City == "México"; })
+                                    .Select(person => person.Name);
+            
+            Console.WriteLine("Personas viviendo en México:");
+            foreach (var PersonName in PersonsOfMexico)
+            {
+                Console.WriteLine(PersonName);
+            }
+        }
+
+        static void UseLinq(System.Collections.Generic.IEnumerable<Person> Persons){
+            Console.WriteLine("Iniciando demo de LINQ...");
+
+            var PersonsOfMexico = Persons
+                                    .Where(person => { ShowThreadCurrentInfo("\"Where city (plinq)\"");
+                                        return person.City == "México"; })
+                                    .Select(person => person.Name);
+            
+            Console.WriteLine("Personas viviendo en México:");
+            foreach (var PersonName in PersonsOfMexico)
+            {
+                Console.WriteLine(PersonName);
+            }
+        }
+
+        static void UseWithDegreeOfParallelism(System.Collections.Generic.IEnumerable<Person> Persons){
+            Console.WriteLine("Iniciando demo de WithDegreeOfParallelism...");
+
+            var PersonsOfMexico = Persons.AsParallel()
+                                    .WithDegreeOfParallelism(3)
+                                    .Where(person => { ShowThreadCurrentInfo("\"Where city (plinq)\"");
+                                        return person.City == "México"; })
+                                    .Select(person => person.Name);
+            
+            Console.WriteLine("Personas viviendo en México:");
+            foreach (var PersonName in PersonsOfMexico)
+            {
+                Console.WriteLine(PersonName);
+            }
+        }
+
+        static void UseWithMergeOptions(System.Collections.Generic.IEnumerable<Person> Persons){
+            Console.WriteLine("Iniciando demo de WithMergeOptions(...");
+
+            var PersonsOfMexico = Persons.AsParallel()
+                                    .WithDegreeOfParallelism(3)
+                                    .WithMergeOptions(System.Linq.ParallelMergeOptions.NotBuffered)
+                                    .Where(person => { ShowThreadCurrentInfo("\"Where\"");
+                                        return person.City == "México"; })
+                                    .Select(person => person.Name);
+            
+            Console.WriteLine("Personas viviendo en México:");
+            foreach (var PersonName in PersonsOfMexico)
+            {
+                Console.WriteLine(PersonName);
+            }
+        }
+
+        static void UseWithCancellation(System.Collections.Generic.IEnumerable<Person> Persons){
+            Console.WriteLine("Iniciando demo de WithCancellation..");
+            System.Threading.CancellationToken Ct;
+            System.Threading.CancellationTokenSource Cts = new System.Threading.CancellationTokenSource();
+            Ct = Cts.Token;
+
+            System.Threading.Tasks.Task.Run(() => {
+                Console.WriteLine("Iniciando tarea canceladora...");
+                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine("Iniciando solicitud de cancelación");
+                Cts.Cancel();
+            });
+
+            var PersonsOfMexico = Persons.AsParallel()
+                                    .WithDegreeOfParallelism(2)
+                                    .WithMergeOptions(System.Linq.ParallelMergeOptions.AutoBuffered)
+                                    .WithCancellation(Ct)
+                                    .Where(person => { ShowThreadCurrentInfo("\"Where\"");
+                                        return person.City == "México"; })
+                                    .Select(person => person.Name);
+            
+            Console.WriteLine("Personas viviendo en México:");
+            foreach (var PersonName in PersonsOfMexico)
+            {
+                Console.WriteLine(PersonName);
+            }
+        }
+
+        static void UseWithExecutionMode(System.Collections.Generic.IEnumerable<Person> Persons){
+            Console.WriteLine("Iniciando demo de WithExecutionMode...");
+
+            var PersonsOfMexico = Persons.AsParallel()
+                                    .WithDegreeOfParallelism(3)
+                                    .WithMergeOptions(System.Linq.ParallelMergeOptions.AutoBuffered)
+                                    .WithExecutionMode(System.Linq.ParallelExecutionMode.Default)
+                                    .Where(person => { ShowThreadCurrentInfo("\"Where\"");
+                                        return person.City == "México"; })
+                                    .Select(person => person.Name);
+            
+            Console.WriteLine("Personas viviendo en México:");
+            foreach (var PersonName in PersonsOfMexico)
+            {
+                Console.WriteLine(PersonName);
+            }
+        }
+
+        static void ShowThreadCurrentInfo(string opertor){
+            Console.WriteLine($"Executing operator: {opertor} in ManageThreadId: " + 
+                $"{System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            //System.Threading.Thread.Sleep(4000);
         }
     }
 }
