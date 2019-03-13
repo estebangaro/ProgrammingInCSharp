2e432d2a4f6e9dc71ad06f990ca33862b817ee3f
Ubuntu <estebangaro@outlook.com>
2019-03-13 04:18:26 +0000

    Se añaden ejemplos de creación, inicialización y espera de tareas

---
+++ Exam Ref 70-483/Task Parallel library/netcore/March19/Tasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingTaskRun();
+        }
+
+        static void DoWork(){
+            Console.WriteLine("Do Working...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Finishing working...");
+        }
+
+        static void UsingTaskConstructor(){
+            Console.WriteLine("Iniciando ejecución de método UsingTaskConstructor");
+            var Task = new System.Threading.Tasks.Task(new Action(DoWork));//Creación de Task.
+            Task.Start();//Inicialización de ejecución.
+            Task.Wait();//Esperar a que tarea concluya.
+            Console.WriteLine("Finalizando ejecución de método UsingTaskConstructor");
+        }
+
+        static void UsingTaskRun(){
+            Console.WriteLine("Iniciando ejecución de método UsingTaskRun");
+            var Task = System.Threading.Tasks.Task.Run(new Action(DoWork));//Creación e inicialización de Task.
+            Task.Wait();//Esperar a que tarea concluya.
+            Console.WriteLine("Finalizando ejecución de método UsingTaskRun");
+        }
+        
+    }
+}
