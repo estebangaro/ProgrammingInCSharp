bb61107c20a1a264b27f8b05bf5ed44c030e29db
Ubuntu <estebangaro@outlook.com>
2019-04-05 05:09:57 +0000

    Se incluyen ejercicios con ParallelOptions en For y ForEach Methods

--- Exam Ref 70-483/Chapter 1 - Manage Program Flow/Skill 1.1 - Implement Multithreading and asyncronous processing/The Task Parallel Library/Tasks.Parallel/Program.cs
+++ Exam Ref 70-483/Chapter 1 - Manage Program Flow/Skill 1.1 - Implement Multithreading and asyncronous processing/The Task Parallel Library/Tasks.Parallel/Program.cs
@@ -9,7 +9,8 @@ namespace Tasks.Parallel
             Console.WriteLine("Hello Parallel Class!");
             //UseInvokeMethod();
             //UseForEeachMethod();
-            UseForMethod();
+            //UseForMethod();
+            UseForAndForEachWithParallelOptions();
         }
 
         static void UseForEeachMethod(){
@@ -35,15 +36,61 @@ namespace Tasks.Parallel
             int FromInclusive = 1, ToExclusive = 26;
             var Result = System.Threading.Tasks.Parallel.For(FromInclusive, ToExclusive, 
                 loopIndex => {
-                    Console.WriteLine("Inciando ejecución de índice de iteración " + loopIndex);
+                    int ManageThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                    Console.WriteLine("Inciando ejecución de índice de iteración " + loopIndex + $", en hilo {ManageThreadId}");
                     System.Threading.Thread.Sleep(loopIndex * 5);
-                    Console.WriteLine("Finalizando ejecución de índice de iteración " + loopIndex);
+                    Console.WriteLine("Finalizando ejecución de índice de iteración " + loopIndex  + $", en hilo {ManageThreadId}");
                 });
             Console.WriteLine("Estado de cliclo For Paralelizado: " + Result.IsCompleted);
         }
 
         static void UseForAndForEachWithParallelOptions(){
-            Console.WriteLine("Iniciando ejecución de For - Method"); 
+            
+            /*Console.WriteLine("Iniciando ejecución de For Method - PO MaxDegreeOfParallelism");
+            var ResultFor = System.Threading.Tasks.Parallel.For(1, 51, new System.Threading.Tasks.ParallelOptions{
+                MaxDegreeOfParallelism = 3
+            }, (loopIndex, loopState) => {
+                int ManageThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine("Inciando ejecución de índice de iteración " + loopIndex + $", en hilo {ManageThreadId}");
+                System.Threading.Thread.Sleep(loopIndex * 5);
+                Console.WriteLine("Finalizando ejecución de índice de iteración " + loopIndex + $", en hilo {ManageThreadId}");
+            });
+            Console.WriteLine("Estado de cliclo For Paralelizado: " + ResultFor.IsCompleted);*/
+
+            System.Threading.CancellationTokenSource Cts = new System.Threading.CancellationTokenSource();
+            System.Threading.CancellationToken Ct = Cts.Token;
+            try{
+                var ResultForEach = System.Threading.Tasks.Parallel.ForEach(System.Linq.Enumerable.Range(1, 50), 
+                    new System.Threading.Tasks.ParallelOptions{
+                        CancellationToken = Ct
+                    }, integer => {
+                        if(Ct.IsCancellationRequested){
+                            Console.WriteLine("Iniciando ejeución con solicitud de cancelación");
+                        }
+                        int ManageThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                        Console.WriteLine(string.Concat( "Trabajando en entero ", integer, $", en hilo {ManageThreadId}"));
+                System.Threading.Thread.Sleep(integer * 10);
+                if(integer == 16){
+                    Console.WriteLine("Por solicitar cancelación");
+                    Cts.Cancel();
+                }
+                //Ct.ThrowIfCancellationRequested();
+                if(!Ct.IsCancellationRequested){
+                Console.WriteLine(string.Concat("Finalizando con entero ", integer, $", en hilo {ManageThreadId}"));
+                }else{
+                    Console.WriteLine(string.Concat("Finalizando con entero ", integer, $", en hilo {ManageThreadId}",
+                    " con manejo de solicitud de cancelación"));
+                }
+                    });
+            }catch(AggregateException ae){
+                Console.WriteLine("Iniciando control de excepciones acumuladas");
+                foreach (Exception item in ae.InnerExceptions)
+                {
+                    Console.WriteLine(item);
+                }
+            }catch(OperationCanceledException ex){
+                Console.WriteLine("Iniciando control de excepción " + ex.Message);
+            }
         }
 
         static void UseInvokeMethod(){
